92815a13177fc9e964ec38ab774576a748c48668
Alex Piggott <apiggott@google.com>
2024-07-29 04:15:55 -0700

    Fixed bugs in run_preprocess_partials.py

--- processing/run_preprocess_partials.py
+++ processing/run_preprocess_partials.py
@@ -4,13 +4,11 @@ import partial_eclipse_tracker
 
 def main():
   tracker = partial_eclipse_tracker.PartialEclipseTracker()
-  #tracker.preprocess_images(max_images=3)
   tracker.preprocess_images()
   tracker.save_preprocess_to_file('preprocess_partials')
 
   new_tracker = partial_eclipse_tracker.PartialEclipseTracker()
   new_tracker.load_preprocess_from_file('preprocess_partials')
 
-  plt.show()
-
 if __name__ == '__main__':
+  main()
